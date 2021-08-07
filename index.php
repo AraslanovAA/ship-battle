6adc41b8d8eb19abb1c6eedf650e31bf15ab79d4
AraslanovAA <yourfriendkesha.sendmemes@gmail.com>
2021-08-07 17:18:01 +0300

    init index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<meta charset="UTF-8">
+    <title>морской бой</title>
+    <link href="./styles.css" rel="stylesheet" type="text/css" />
+
+    </head>
+<body>
+<table id="firstT">
+    <tbody>
+        <?php 
+        for($i = 0; $i <10 ; $i++){
+            echo "<tr>";
+            for($j=0;$j<10;$j++){
+                echo "<td class=''></td>";
+            }
+            echo "</tr>";
+        }
+
+
+        ?>
+        
+</tbody></table>
+
+
+</body>
+
+</html>
