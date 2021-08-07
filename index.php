02cd81646873d050b07a5fdffae5dee7d0003773
AraslanovAA <yourfriendkesha.sendmemes@gmail.com>
2021-08-07 21:39:57 +0300

    номер нажатой клетки передаётся через post

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang="ru">
 <head>
+
+<meta charset="UTF-8">
+    <title>морской бой</title>
     <link href="./style3.css" rel="stylesheet" type="text/css" />
 
     </head>
@@ -14,15 +17,19 @@ try {
     } catch (Exception $e) {
     $error  = $e->getMessage();
     }
-?>
 
+    if (isset ($_POST['clickedCell'])){
+        echo $_POST['clickedCell'];
+    }
+?>
+<form method="POST" action="">  
 <table>
 <caption>поле противника</caption>
         <?php 
         for($i = 0; $i <10 ; $i++){
             echo "<tr>";
             for($j=0;$j<10;$j++){
-                echo "<td class = '" . $game->getCssClass(10*$i + $j) . "'><a href='./index.php?move=" . (10*$i + $j) ."'></a></td>";
+                echo "<td class = '" . $game->getCssClass(10*$i + $j) . "'> <input type='submit' name = 'clickedCell' value='".(10*$i + $j)."'></td>";
             }
             echo "</tr>";
         }
@@ -31,7 +38,7 @@ try {
         ?>
         
 </table>
-
+</form>
 
 </body>
 
