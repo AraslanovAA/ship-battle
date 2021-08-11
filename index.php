b6e7085eac9cb16603db4086764adb081503e361
AraslanovAA <yourfriendkesha.sendmemes@gmail.com>
2021-08-11 15:02:45 +0300

    Полностью реализована клиентская часть
    index.php главный файл возвращающий пользователю страницу
    файл logic.php определяет логику игры
    при нажатии на клетку поля отправляется post запрос php с номером нажатой клетки $_POST['clickedCell']
    php отправляет координаты выстрела на сервер node через post, результат выстрела записывается в данные сессии
    при создании страницы php берёт данные из сессии и устанавливает стили ячеек в зависимости от состояния клетки
    стилям соответствуют картинки с изображениями соответствующих состояний корабля

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 session_start();
+/*
+файл logic.php определяет логику игры
+при нажатии на клетку поля отправляется post запрос php с номером нажатой клетки $_POST['clickedCell']
+php отправляет координаты выстрела на сервер node через post, результат выстрела записывается в данные сессии
+при создании страницы php берёт данные из сессии и устанавливает стили ячеек в зависимости от состояния клетки
+стилям соответствуют картинки с изображениями соответствующих состояний корабля
+*/
 ?>
 
 <!DOCTYPE html>
@@ -19,30 +26,81 @@ try {
     $game = new Logic();
     $game->setData($_GET);
     $game->reset();
-    //$game->testCURL();
+    
+    $checkWin = $game->checkWin();
+    if($checkWin){
+        if($game->getUserShips() == 0){
+            echo 'Вы выиграли';
+            }
+            else{
+                echo 'Вы проиграли';
+            }
+    }
+    else{
     if (isset ($_POST['clickedCell'])){
-        echo $game->userMove($_POST['clickedCell']);
+        $game->userMove($_POST['clickedCell']);
+    }}
+    $checkWin = $game->checkWin();
+    if($checkWin){
+        if($game->getUserShips() == 0){
+        echo 'Вы выиграли';
+        }
+        else{
+            echo 'Вы проиграли';
+        }
     }
     } catch (Exception $e) {
     $error  = $e->getMessage();
     }
     
 ?>
+<div class="container">
 <form method="POST" action="">  
 <table>
-<caption>поле противника</caption>
+<caption>кораблей противника: <?php echo $game->getUserShips() ?></caption>
         <?php 
+        if(!$checkWin){
         for($i = 0; $i <10 ; $i++){
             echo "<tr>";
             for($j=0;$j<10;$j++){
-                $className = $game->getCssClass(10*$i + $j);
+                $className = $game->getUserCssClass(10*$i + $j);
                 if($className == "water"){
                     echo "<td class = 'water'> <input type='submit' class ='opacityFalse' name = 'clickedCell' value='".(10*$i + $j)."'></td>";
                 }
                 else{
-                    echo "<td class = '" . $game->getCssClass(10*$i + $j) . "'><input type='submit' class ='visibleFalse' name = 'clickedCell' value='".(10*$i + $j)."'></td>";
+                    echo "<td class = '" . $game->getUserCssClass(10*$i + $j) . "'><input type='submit' class ='visibleFalse' name = 'clickedCell' value='".(10*$i + $j)."'></td>";
                 }
                 
+            }
+            echo "</tr>";
+        }
+        }
+        else{
+            for($i = 0; $i <10 ; $i++){
+                echo "<tr>";
+                for($j=0;$j<10;$j++){
+                      echo "<td class = '" . $game->getUserCssClass(10*$i + $j) . "'><a href=" .'"./index.php?reset"'." ></a></td>";
+                }
+                echo "</tr>";
+            }
+        }
+
+        ?>
+        
+</table>
+
+
+</form>
+
+<form method="POST" action="">  
+<table>
+<caption>ваших кораблей: <?php echo $game->getServerShips() ?></caption>
+        <?php 
+        for($i = 0; $i <10 ; $i++){
+            echo "<tr>";
+            for($j=0;$j<10;$j++){
+                echo "<td class = '" . $game->getServerCssClass(10*$i + $j) . "'><input type='hidden' class ='visibleFalse'></td>";
+              
             }
             echo "</tr>";
         }
@@ -51,11 +109,26 @@ try {
         ?>
         
 </table>
-<br><br>
-<a href="./index.php?reset" >Начать сначала</a>
 
 </form>
 
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+<?php
+if ($checkWin){
+echo '<a href="./index.php?reset" >Новая игра</a>';
+} ?>
 </body>
 
 </html>
--- logic.php
+++ logic.php
@@ -5,7 +5,7 @@ class Logic//класс с логикой игры
 {
     protected $data_inside;
     public function __construct() {
-        if (!isset($_SESSION['curgame'])) {
+        if (!isset($_SESSION['player_field'])) {
             $this->resetGame();
         }
         
@@ -24,54 +24,42 @@ class Logic//класс с логикой игры
         }
 
     }
-    public function testCURL(){
-        $url = "localhost:3000/shoot";
-
-        $post_data = array (
-            "foo" => "bar",
-            "query" => "Nettuts",
-            "action" => "Submit"
-        );
-        $json = json_encode($post_data);
-        echo $json;
-        echo '<br>';
-        echo $post_data;
-        echo '<br>';
-        $ch = curl_init();
-        $json2 = '';
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        
-        // Добавляем переменные
-        //curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
-        $payload = json_encode( array( "customer"=> $post_data ) );
-        $coord = '123';
-        curl_setopt( $ch, CURLOPT_POSTFIELDS, $json );
-        curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
-        $output = curl_exec($ch);
-
-        curl_close($ch);
-
-        echo $output;
-        echo '<br>';   
-        $obj = json_decode($output, true);
-        echo $obj['victory']; // успешно получает данные из запроса
-    }
+    
     protected function resetGame()
     {
-        $_SESSION['curgame'] = array_fill(0, 100, 'water');
+        $_SESSION['player_field'] = array_fill(0, 100, 'water');
+        $_SESSION['server_field'] = array_fill(0, 100, 'water');
+        $_SESSION['server_ships'] = 10;
+        $_SESSION['player_ships'] = 10;
+        $_SESSION['stopGame'] = false;
         if (!isset($_SESSION['name'])) {
             $_SESSION['name'] = uniqid();//дадим каждому по уникальному имени
         }
 
     }
-
-
-    public function getCssClass(int $cellNum): string
+    public function checkWin() : bool {
+        return  $_SESSION['stopGame'];
+    }
+    public function getUserShips() : int
     {
-        return $_SESSION['curgame'][$cellNum];
+        return $_SESSION['player_ships'];
+    }
+    public function getServerShips() : int
+    {
+        return $_SESSION['server_ships'];
+    }
+
+    public function getUserCssClass(int $cellNum): string
+    {
+        return $_SESSION['player_field'][$cellNum];
     }        
+
+    public function getServerCssClass(int $cellNum): string
+    {
+        return $_SESSION['server_field'][$cellNum];
+    }        
+
+    
 
     public function userMove(int $cellNum) {//имитируем обращение к серверу пока
 //функ должна вернуть по сути количество кораблей оставшихся, и результат попадания 
@@ -94,24 +82,67 @@ class Logic//класс с логикой игры
                 
         $output = curl_exec($ch);
         curl_close($ch);
-
-        echo $output;
-        echo '<br>вот выше резалт получается';   
+ 
         $obj = json_decode($output, true);
-        //echo $obj['victory']; // успешно получает данные из запроса
+
+        if(array_key_exists('shoot_result', $obj)){
         if($obj['shoot_result'] == 'hit'){
-            $_SESSION['curgame'][$cellNum] = 'hit';
+            $_SESSION['player_field'][$cellNum] = 'hit';
         }
         if($obj['shoot_result'] == 'miss'){
-            $_SESSION['curgame'][$cellNum] = 'miss';
+            $_SESSION['player_field'][$cellNum] = 'miss';
         }
         if($obj['shoot_result'] == 'kill'){
-            $_SESSION['curgame'][$cellNum] = 'kill';
+            //$_SESSION['player_field'][$cellNum] = 'kill'      
             for($i =0; $i<count($obj['cell']);$i++){
-                $_SESSION['curgame'][$obj['cell'][$i]] = 'kill';
+                $_SESSION['player_field'][$obj['cell'][$i]] = 'kill';
+            }
+            $this->postNumShips();
+            
+        }
+        }
+
+        if(array_key_exists('server_shoot', $obj)){
+            if($obj['server_shoot'] == 'hit'){
+                $_SESSION['server_field'][$obj['server_cell']] = 'hit';
+            }
+            if($obj['server_shoot'] == 'miss'){
+                $_SESSION['server_field'][$obj['server_cell']] = 'miss';
+            }
+            if($obj['server_shoot'] == 'kill'){
+                for($i =0; $i<count($obj['server_cell']);$i++){
+                    $_SESSION['server_field'][$obj['server_cell'][$i]] = 'kill';
+                }
+                $this->postNumShips();
             }
         }
-    }
 
+
+    }
+    public function postNumShips(){
+        $url = "localhost:3000/numShips";
+            
+            $post_data = array (
+                "user" => $_SESSION['name']
+            );
+            $json = json_encode($post_data);
+            $ch = curl_init();
+            
+            curl_setopt( $ch, CURLOPT_POSTFIELDS, $json );
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+                    
+            $output = curl_exec($ch);
+            curl_close($ch);
+            $obj2 = json_decode($output, true);
+            $_SESSION['server_ships'] = $obj2['NumServerShips'];
+            $_SESSION['player_ships'] = $obj2['numUserShips'];
+            if(($_SESSION['server_ships'] == 0) || ($_SESSION['player_ships'] == 0)){
+                $_SESSION['stopGame'] = true;//если у одного из игроков законились корабли поднимаем флажок конца игры
+            }
+
+   }
    
 }
