6167dba728e96fe8aae88c6c9fe93754157c5e21
AraslanovAA <yourfriendkesha.sendmemes@gmail.com>
2021-08-19 06:55:20 +0300

    подключение vue

---
+++ App.vue
@@ -0,0 +1,16 @@
+<template>
+  
+  <enemyTable />
+</template>
+
+<script>
+import enemyTable from './components/enemyTable.vue'
+
+export default {
+  name: 'App',
+  components: {
+    enemyTable
+  }
+}
+</script>
+
---
+++ enemyTable.vue
@@ -0,0 +1,41 @@
+<template>
+  
+  <table>
+    <caption>кораблей противника: {{enemyShips}}</caption>
+    <tbody>
+      <tr v-for="(row,indexRow) in enemyTable" :key="indexRow">
+        <td v-for="(cell,indElemInRow) in row" :key="indElemInRow" @click="change(indexRow,indElemInRow)">{{cell}} </td>
+      </tr>
+    </tbody>
+  </table>
+  
+</template>
+
+<script>
+export default {
+  name: 'enemyTable',
+    data(){
+    return {
+    enemyShips : 10,
+    enemyTable : []
+    }
+  },
+  created(){//
+    for(let i = 0; i < 10;i++){
+      let td = []
+      for(let j = 0; j< 10;j++){
+        td.push('water');
+      }
+      this.enemyTable.push(td);
+    }
+     
+  },
+  methods:{
+    change(indexRow,indexElem){
+      this.enemyTable[indexRow][indexElem]='hit';
+    }
+  }
+}
+</script>
+
+
